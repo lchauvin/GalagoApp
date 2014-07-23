b9b9623a5b831514a5803cff7f04b42a8c120ae3
lchauvin <lchauvin@bwh.harvard.edu>
2014-07-23 16:59:49 -0400

    ENH: First release version

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -6,20 +6,12 @@ module.exports = [
         "clobbers": [
             "navigator.splashscreen"
         ]
-    },
-    {
-        "file": "plugins/com.kernix.pdfviewer/www/pdfviewer.js",
-        "id": "com.kernix.pdfviewer.PDFViewer",
-        "clobbers": [
-            "PDFViewer"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "org.apache.cordova.splashscreen": "0.3.0",
-    "com.kernix.pdfviewer": "1.1.0"
+    "org.apache.cordova.splashscreen": "0.3.0"
 }
 // BOTTOM OF METADATA
 });
